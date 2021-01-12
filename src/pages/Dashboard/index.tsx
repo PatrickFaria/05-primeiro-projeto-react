bd2eeb2f0d24ab9b9f0f86254fae9c12f5e69fe4
PatrickFaria <patrickarfaria@gmail.com>
2021-01-12 09:03:09 -0300

    finaliza a estilização do dashboard e inicia a chamada da api do github

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,47 @@
-import React from 'react';
-import { Title } from './styles';
+import React, { useState } from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github </Title>;
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState({});
+
+  function handleAddRepository(): void {
+    console.log(newRepo);
+  }
+
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github </Title>
+      <Form onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={e => setNewRepo(e.target.value)}
+          placeholder="Digite o nome do repositório"
+        />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/19639217?s=460&u=ddc0740ffbd0e72660087cfbf56b568b9ea0634d&v=4"
+            alt="Patrick Faria"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Descrição</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
